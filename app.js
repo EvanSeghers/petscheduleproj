3febe7cb4278d9202c255061ba54a7c2472a0192
Clint Woerishofer <woerishoferclinten@gmail.com>
2019-12-12 18:34:24 -0800

    Backend Works

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const path = require('path');
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const config = require('./config');
-
+const bcrypt = require("bcrypt");
+const port = process.env.PORT || 5000;
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+require("./passport-config")(passport);
+const methodOverride = require("method-override");
+const mysql = require('mysql')
 const app = express();
 
 // Express configuration.
@@ -12,18 +19,56 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.set('trust proxy', true);
 
-// Customers routes.
-app.use('/customers', require('./customers/crud'));
+//Connect to DB
 
-app.use('/schedule', require('./schedule/crud'));
+/*var con = mysql.createConnection({dbconfig});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    isconnected = true;
+})*/
+
+
+//Initialize Middleware
+//Allows us to access form variables inside post methods
+app.use(express.urlencoded({ extended: false }));
+app.use( express.static( "public" ) );
+
+app.use(flash());
+app.use(
+  session({
+    secret: "secret", 
+    //do not want to resave session variables if nothing has changed
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Using delete is safer than using POST
+app.use(methodOverride("_method"));
+
 
 // Index route.
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
+app.get("/login",(req, res) => {
+    res.render("login.ejs");
+  });
+
+
+  app.get("/test",(req, res) => {
+    res.render("login.ejs");
+  });
+
+
 // Start the server.
-const server = app.listen(config.get('PORT'), () => {
+const server = app.listen(port, () => {
     const port = server.address().port;
     console.log(`App listening on ${port}`);
 });
